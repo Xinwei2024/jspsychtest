a9f178d74c4b4ef344a4ce21734dbac8bd06010e
Xinwei2024 <13426694750@163.com>
2024-05-19 17:37:08 +0800

    Add files via upload

---
+++ exp.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function() {
+    Papa.parse('path_to_your_csv_file.csv', {
+        download: true,
+        header: true,
+        complete: function(results) {
+            var timeline = [];
+
+            const instructions = {
+                type: 'html-keyboard-response',
+                stimulus: '在每个词汇出现后，如果你认为这是一个真实存在的词，请按"F"键；如果认为这是一个虚构的词，请按"J"键。按任意键开始。',
+                post_trial_gap: 1000
+            };
+            timeline.push(instructions);
+
+            const test_procedure = {
+                timeline: [{
+                    type: 'html-keyboard-response',
+                    stimulus: jsPsych.timelineVariable('stimulus'),
+                    choices: ['f', 'j'],
+                    data: {
+                        correct_response: jsPsych.timelineVariable('correct_response')
+                    },
+                    on_finish: function(data) {
+                        data.correct = data.response === data.correct_response;
+                    }
+                }],
+                timeline_variables: results.data,
+                randomize_order: true,
+                repetitions: 1
+            };
+            timeline.push(test_procedure);
+
+            jsPsych.init({
+                timeline: timeline,
+                on_finish: function() {
+                    jsPsych.data.displayData();
+                }
+            });
+        }
+    });
+});
